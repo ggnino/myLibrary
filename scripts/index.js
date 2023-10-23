c21ac999c3d003358cd84eac16dd46c0b7fe03f8
Guillermo Gonzalez <ggnino18@gmail.com>
2023-10-23 02:07:38 -0400

    Added Some Validation For Modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// const myLibrary = [];
-
 function Books(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -9,7 +7,7 @@ function Books(title, author, pages, read) {
     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
   };
 }
-
+let userBook = new Books();
 function createMyBooks() {
   const book1 = createBookElements(
     new Books("Eloquent JavaScript", "Marijn Haverbeke", 474, "Read")
@@ -46,7 +44,7 @@ function createBookElements(book) {
   const myBookPages = document.createElement("p");
   const myBookRead = document.createElement("p");
   const myBookBtn = document.createElement("button");
-  let bookClassName = "library-book";
+  const bookClassName = "library-book";
 
   myBook.setAttribute("class", bookClassName);
   myBookTitle.setAttribute("class", `${bookClassName}-title`);
@@ -71,21 +69,78 @@ function addBookToLibrary(book = null) {
   const myBookShelf = document.getElementsByClassName("library-bookshelf")[0];
 
   const myLibrary = createMyBooks();
+  eventAdder();
   if (!book) myBookShelf.append(...myLibrary);
-  else myBookShelf.append(book);
 }
 
 function clickHandler(e) {
   const dialog = document.getElementsByTagName("dialog")[0];
+  const form = document.getElementsByClassName("library-modal-form")[0];
+  const closeBtn = document.getElementById("modal-close-btn");
+
+  console.log(e.target.id);
 
   if (e.target.className.includes("add")) dialog.showModal();
-  else dialog.close();
+  else {
+    const myBookShelf = document.getElementsByClassName("library-bookshelf")[0];
+    const myLibrary = document.getElementsByClassName("library-book");
+    let inLibrary = false;
+    for (let book of myLibrary) {
+      if (
+        book.children[0].textContent.toLowerCase() ===
+        userBook.title.toLowerCase()
+      ) {
+        inLibrary = true;
+      }
+    }
+    if (!inLibrary) {
+      myBookShelf.appendChild(createBookElements(userBook));
+      closeBtn.disabled = true;
+      form.reset();
+      dialog.close();
+    } else {
+      closeBtn.disabled = true;
+      form.reset();
+    }
+
+    for (let prop in userBook) {
+      if (userBook[prop]) userBook[prop] = "";
+    }
+  }
 }
-document
-  .getElementsByClassName("library-add-btn")[0]
-  .addEventListener("click", (e) => clickHandler(e));
-document
-  .getElementById("modal-close-btn")
-  .addEventListener("click", (e) => clickHandler(e));
+
+function eventAdder() {
+  const addBtn = document.getElementsByClassName("library-add-btn")[0];
+  const closeBtn = document.getElementById("modal-close-btn");
+  const titleInput = document.getElementById("book_title");
+  const authorInput = document.getElementById("book_author");
+  const pagesInput = document.getElementById("book_pages");
+  const readInput = document.getElementById("book_read");
+  const deleteBtns = document.getElementsByClassName("library-book-btn");
+  for (let btn of deleteBtns) {
+    btn.addEventListener("click", (e) => clickHandler(e));
+  }
+  closeBtn.disabled = true;
+  addBtn.addEventListener("click", (e) => clickHandler(e));
+  closeBtn.addEventListener("click", (e) => clickHandler(e));
+  titleInput.addEventListener("input", (e) => inputHandler(e));
+  authorInput.addEventListener("input", (e) => inputHandler(e));
+  pagesInput.addEventListener("input", (e) => inputHandler(e));
+  readInput.addEventListener("input", (e) => inputHandler(e));
+}
+
+function inputHandler(e) {
+  if (e.target.id === "book_title") userBook.title = e.target.value;
+  if (e.target.id === "book_author") userBook.author = e.target.value;
+  if (e.target.id === "book_pages") userBook.pages = e.target.value;
+  if (e.target.id.includes("read")) userBook.read = e.target.value;
+  if (userBook.author && userBook.title && userBook.pages) {
+    console.log("aqui");
+    const closeBtn = document.getElementById("modal-close-btn");
+    closeBtn.disabled = false;
+  }
+  console.log(userBook.author, userBook.title, userBook.pages);
+  console.log(userBook.author && userBook.title && userBook.pages);
+}
 
 addBookToLibrary();
